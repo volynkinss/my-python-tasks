0949554c683cc31abefb3b26714252fa2d57e6b8
volynkinss <volynkin_s@icloud.com>
2023-03-15 17:48:26 +0300

    changed the program by comparing it to task 2 and add comments

--- Ex2.py
+++ Ex2.py
@@ -1,4 +1,5 @@
-s = input("Введите текст: ")
-s = s[::-1]
-print(s)
-
+# need input two numbers and print sum of it
+a = float(input("Введите первое число: ")) #input 1st number
+b = float(input("Введите второе число: ")) #input 2nd number
+c = a+b #calculate sum of 2 numbers
+print ("Сумма двух чисел равна", c) #print sum of numbers
