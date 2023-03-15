27b9cf6a4950a9728c66166a24ea69ce9089cb64
volynkinss <volynkin_s@icloud.com>
2023-03-15 17:37:26 +0300

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,5 @@
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
-c = a+b
-print ("Сумма двух чисел равна", c)
+# need input two numbers and print sum of it
+a = float(input("Введите первое число: ")) #input 1st number
+b = float(input("Введите второе число: ")) #input 2nd number
+c = a+b #calculate sum of 2 numbers
+print ("Сумма двух чисел равна", c) #print sum of numbers
