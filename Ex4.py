10ea70999799652d491664bd9e116a9f4c9e7707
volynkinss <volynkin_s@icloud.com>
2023-03-16 13:48:45 +0300

    fix an error when entering a string or fractional number and restarts the function

--- Ex4.py
+++ Ex4.py
@@ -1,4 +1,8 @@
 #create program that input list of numbers and calculate amount 
-nums = [int(x)for x in input().split()]
-amount = sum(nums)
-print(amount)
+while True:
+    try:
+        nums = [int(x)for x in input("Введите несколько чисел через пробел: ").split()]
+        amount = sum(nums)
+        print(amount)
+        break
+    except: print("Необходимо вводить целые числа")
