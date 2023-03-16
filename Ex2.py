b486b98e9344ed3057d8332dfe6d546f70328bea
volynkinss <volynkin_s@icloud.com>
2023-03-16 18:44:47 +0300

    add check string contains only whitespace or nothing

--- Ex2.py
+++ Ex2.py
@@ -1,10 +1,12 @@
 # ex2 - sum two numbers
 while True:
     try:
-        a = float(input("Введите первое число: ")) #input 1st number
-        b = float(input("Введите второе число: ")) #input 2nd number
-        c = a+b #calculate sum of 2 numbers
-        print ("Сумма двух чисел равна", c) #print sum of numbers
+        a = input("Введите первое число: ") #input 1st number
+        b = input("Введите второе число: ") #input 2nd number
+        if (a and a.strip()) and (b and b.strip()):
+            c = float(a) + float(b) #calculate sum of 2 numbers
+            print ("Сумма двух чисел равна", c) #print sum of number
+        else: print("Поле не может быть пустым")
         break
     except:
         print("Необходимо вводить только числа")
