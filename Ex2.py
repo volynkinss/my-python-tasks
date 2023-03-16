965a7c21d1a5fa17b0b84460e306f77a911f2342
volynkinss <volynkin_s@icloud.com>
2023-03-16 13:58:13 +0300

    fix an error when entering a string or fractional number and restarts the function

--- Ex2.py
+++ Ex2.py
@@ -1,5 +1,10 @@
 # need input two numbers and print sum of it
-a = float(input("Введите первое число: ")) #input 1st number
-b = float(input("Введите второе число: ")) #input 2nd number
-c = a+b #calculate sum of 2 numbers
-print ("Сумма двух чисел равна", c) #print sum of numbers
+while True:
+    try:
+        a = float(input("Введите первое число: ")) #input 1st number
+        b = float(input("Введите второе число: ")) #input 2nd number
+        c = a+b #calculate sum of 2 numbers
+        print ("Сумма двух чисел равна", c) #print sum of numbers
+        break
+    except:
+        print("Необходимо вводить только числа")
