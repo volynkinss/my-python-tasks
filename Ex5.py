41ebc782d2db1902bd42495395b79bfb35fa58ba
volynkinss <volynkin_s@icloud.com>
2023-03-16 13:43:06 +0300

    fix an error when entering a string or fractional number and restarts the function

--- Ex5.py
+++ Ex5.py
@@ -1,7 +1,12 @@
 #program for print multiplication table of number entered by the user
-num = int(input())
-#prod = [[num] for num in range(1,11)]
-prod = list(range(1,11))
-for i,elem in enumerate(prod):
-        prod[i] *=num
-print(prod)
+while True:
+    try:
+        num = int(input("Введите целое число: "))
+        prod = list(range(1,11))
+        for i,elem in enumerate(prod):
+            prod[i] *=num
+        print(prod)
+        break
+    except:
+        print("Необходимо ввести целое число")
+    
