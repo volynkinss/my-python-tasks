5ffbafa87104dd324532b95792accb8ad19693d3
volynkinss <volynkin_s@icloud.com>
2023-03-21 22:11:52 +0300

    formatted code

--- Ex2.py
+++ Ex2.py
@@ -1,6 +1,6 @@
 # ex2 - sum two numbers
 def get_elements_from_input(size):
-    print ("количество элементов в массиве "  + str(size))
+    print("количество элементов в массиве " + str(size))
     list = []
     while len(list) < size:
         input_element = input("Введите число ")
@@ -11,11 +11,11 @@ def get_elements_from_input(size):
 
 
 try:
-   element_count = input("Введи количество элементов в массиве ")
-   element_count_size = int(element_count)
-   array_of_elements = get_elements_from_input(element_count_size)
-   result = sum(array_of_elements)
-   print("Сумма введенных чисел  равна " , result)
+    element_count = input("Введи количество элементов в массиве ")
+    element_count_size = int(element_count)
+    array_of_elements = get_elements_from_input(element_count_size)
+    result = sum(array_of_elements)
+    print("Сумма введенных чисел  равна ", result)
 except Exception as e:
-        print("Необходимо вводить только числа")
-        print(e)
+    print("Необходимо вводить только числа")
+    print(e)
