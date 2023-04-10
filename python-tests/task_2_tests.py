2ca3e39ebf20641039008b807c3a09b68f8ab2c7
volynkinss <volynkin_s@icloud.com>
2023-04-10 23:10:44 +0300

    test function get_elements

--- python-tests/task_2_tests.py
+++ python-tests/task_2_tests.py
@@ -1,7 +1,15 @@
 import sys
 
 sys.path.insert(0, "/Users/ludocka/Desktop/IlyaTeachMe/my-python-tasks")
-
 import Ex2
+import unittest
 
-Ex2.main()
+
+class TestEx2(unittest.TestCase):
+    def test_get_elements_from_input(self):
+        result = sum(Ex2.get_elements_from_input(1))
+        self.assertEqual(result, 12)
+
+
+if __name__ == "__main__":
+    unittest.main()
