fa4ea1b637775533dc0e38835713b7a92d009d5a
volynkinss <volynkin_s@icloud.com>
2023-04-25 12:59:01 +0300

    create two tests for Ex2. Test is OK

--- python-tests/task_2_tests.py
+++ python-tests/task_2_tests.py
@@ -1,14 +1,15 @@
 import sys
+import unittest
 
 sys.path.insert(0, "/Users/ludocka/Desktop/IlyaTeachMe/my-python-tasks")
 import Ex2
-import unittest
 
 
 class TestEx2(unittest.TestCase):
-    def test_get_elements_from_input(self):
-        result = sum(Ex2.get_elements_from_input(1))
-        self.assertEqual(result, 12)
+    def test_get_array(self):
+        self.assertEqual(Ex2.get_array(1, 4, 5, 2, 0), 12)
+        with self.assertRaises(NameError):
+            Ex2.get_array(Б)
 
 
 if __name__ == "__main__":
