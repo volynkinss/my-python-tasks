1437da1e0816ccdd32ba585929e19fa605e073a7
volynkinss <volynkin_s@icloud.com>
2023-04-25 12:18:36 +0300

    done only one function without cycle, unused code as comment

--- Ex2.py
+++ Ex2.py
@@ -1,22 +1,20 @@
 # ex2 - sum two numbers
 
 
-def get_elements_from_input(size):
-    print("количество элементов в массиве " + str(size))
-    list = []
-    while len(list) < size:
-        input_element = input("Введите число ")
-        input_number = int(input_element)
-        list.append(input_number)
+# def get_elements_from_input(size):
+#     print("количество элементов в массиве " + str(size))
+#     list = []
+#     while len(list) < size:
+#         input_element = input("Введите число ")
+#         input_number = int(input_element)
+#         list.append(input_number)
 
-    return list
+#     return list
 
 
-def get_array_size():
+def get_array_size(*array_of_elements: list):
     try:
-        number_of_elements = input("Введи количество элементов в массиве ")
-        array_size = int(number_of_elements)
-        array_of_elements = get_elements_from_input(array_size)
+        #        array_of_elements = get_elements_from_input(array_size)
         result = sum(array_of_elements)
         print("Сумма введенных чисел равна ", result)
     except ValueError:
@@ -25,9 +23,11 @@ def get_array_size():
         print("Произошла ошибка: ", e)
 
 
-def main():
-    get_array_size()
+# def main():
+#     get_array_size()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+get_array_size(1, 2, 3, 4, 5)
