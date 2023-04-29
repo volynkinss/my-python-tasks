6ca7faf6ca3646d6e2f3c28c1e2e3c2d8fe3ea73
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-04-29 15:02:22 +0300

    added coordinates as argument of function

--- weather.py
+++ weather.py
@@ -1,12 +1,15 @@
 import requests
 import json
 
-def get_weather():
+def get_weather(latitude,longitude):
     url = 'https://api.open-meteo.com/v1/forecast?latitude=59.94&longitude=30.31&current_weather=true'
-    r = requests.get(url)
+    coordinates = {"latitude": "", "longitude":"","current_weather":"true"}
+    coordinates["latitude"]=latitude
+    coordinates["longitude"]=longitude
+    r = requests.get('https://api.open-meteo.com/v1/forecast', params=coordinates)
     weather = r.json()['current_weather']
     return weather
 
 if __name__ == '__main__':
-    current_weather = get_weather()
+    current_weather = get_weather(59.94,30.31)
     print(f"Temperature on St.Petersburg now is a {current_weather['temperature']} degrees Celsius")
