47f2bf80a44a992bd4e19df98fdf0d109b419706
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-04-29 15:17:57 +0300

    added coordinates for spb and moscow

--- weather.py
+++ weather.py
@@ -1,15 +1,20 @@
 import requests
 import json
 
-def get_weather(latitude,longitude):
-    url = 'https://api.open-meteo.com/v1/forecast?latitude=59.94&longitude=30.31&current_weather=true'
+def get_weather(city):
     coordinates = {"latitude": "", "longitude":"","current_weather":"true"}
-    coordinates["latitude"]=latitude
-    coordinates["longitude"]=longitude
+    if city.lower() == "spb":
+        coordinates["latitude"]=59.94
+        coordinates["longitude"]=30.31
+    elif city.lower() == "msk":
+        coordinates["latitude"]=55.75
+        coordinates["longitude"]=37.62
+    else:
+        print("You can choose only 'SPB' or 'MSK'")
     r = requests.get('https://api.open-meteo.com/v1/forecast', params=coordinates)
     weather = r.json()['current_weather']
     return weather
 
 if __name__ == '__main__':
-    current_weather = get_weather(59.94,30.31)
+    current_weather = get_weather("Msk")
     print(f"Temperature on St.Petersburg now is a {current_weather['temperature']} degrees Celsius")
