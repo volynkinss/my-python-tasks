f2093e30f52c0b32b4b3382c5520fe84d5c39301
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-02 15:52:56 +0300

    added class City_characteristics

--- weather.py
+++ weather.py
@@ -6,20 +6,23 @@ list_of_cities = {
             "msk":("Moscow", "55.75", "37.62"),
             "muc":("Munich", "48.14", "11.58")
             }
+
 def get_weather(city):
     try:
-        coordinates = {"latitude": "", "longitude":"","current_weather":"true"}
-        town = ""
-        coordinates["latitude"]=list_of_cities[city.lower()][1]
-        coordinates["longitude"]=list_of_cities[city.lower()][2]
-        town = list_of_cities[city.lower()][0]
+        city = city.lower()
+        class City_characteristics:
+            name = list_of_cities[city][0]
+            latitude = list_of_cities[city][1]
+            longitude = list_of_cities[city][2]
+        city = City_characteristics()
+        coordinates = {"latitude": city.latitude, "longitude":city.longitude,"current_weather":"true"}
         r = requests.get('https://api.open-meteo.com/v1/forecast', params=coordinates)
         weather = r.json()['current_weather']
-        print(f"Temperature in {town} now is a {weather['temperature']} degrees Celsius")
+        print(f"Temperature in {city.name} now is a {weather['temperature']} degrees Celsius")
     except KeyError:
         print("This city is not on the list ")
     except Exception as er:
         print(er)
 
 if __name__ == '__main__':
-    get_weather("muc")
+    get_weather("spb")
