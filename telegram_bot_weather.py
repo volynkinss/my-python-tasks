d3dbcf635960273d97d046e1b5dc41b9f37b6c14
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-02 12:46:58 +0300

    hidden token

--- telegram_bot_weather.py
+++ telegram_bot_weather.py
@@ -1,23 +1,24 @@
 from aiogram import Bot, Dispatcher, executor, types
 from weather import get_weather
+import token_for_bot
 
-bot = Bot(token="6107139755:AAHSRA8PA35xhTxfC2g9bdcIF_y72OMnSl0")
+bot = Bot(token=token_for_bot.token_bot)
 dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
     await message.reply("Hello! I'm weather telegram bot and I can show actually weather for you. Enter '/spb' for see temperature in St.Petersburg, '/msk' for see temperature in Moscow and ‘/muc’ for see temperature in Munich")
 
-@dp.message_handler(commands=['spb', 'SPB'])
+@dp.message_handler(commands=['spb'])
 async def weather_spb(message: types.Message):
     current_weather = get_weather("spb")
     await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
 
-@dp.message_handler(commands=['msk', 'MSK'])
+@dp.message_handler(commands=['msk'])
 async def weather_spb(message: types.Message):
     current_weather = get_weather("msk")
     await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
 
-@dp.message_handler(commands=['muc', 'MUC'])
+@dp.message_handler(commands=['muc'])
 async def weather_spb(message: types.Message):
     current_weather = get_weather("muc")
     await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
