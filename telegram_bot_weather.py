67a9055eea4f7b243c2c6fcb41bba6d082cf313f
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-02 13:34:31 +0300

    Removed repeated functions

--- telegram_bot_weather.py
+++ telegram_bot_weather.py
@@ -8,19 +8,9 @@ dp = Dispatcher(bot)
 async def welcome(message: types.Message):
     await message.reply("Hello! I'm weather telegram bot and I can show actually weather for you. Enter '/spb' for see temperature in St.Petersburg, '/msk' for see temperature in Moscow and ‘/muc’ for see temperature in Munich")
 
-@dp.message_handler(commands=['spb'])
+@dp.message_handler(commands=['spb', 'msk', 'muc'])
 async def weather_spb(message: types.Message):
-    current_weather = get_weather("spb")
-    await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
-
-@dp.message_handler(commands=['msk'])
-async def weather_spb(message: types.Message):
-    current_weather = get_weather("msk")
-    await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
-
-@dp.message_handler(commands=['muc'])
-async def weather_spb(message: types.Message):
-    current_weather = get_weather("muc")
+    current_weather = get_weather(message.text[1:])
     await message.reply(f"Temperature in {current_weather[-1]} now is a {current_weather[0]['temperature']} degrees Celsius")
 
 @dp.message_handler()
