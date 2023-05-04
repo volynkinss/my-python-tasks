7a7c76517408e048d5907057f1807cb1338059fa
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-04 15:15:37 +0300

    push changes

--- telegram_bot_weather.py
+++ telegram_bot_weather.py
@@ -5,12 +5,22 @@ from aiogram.types import Message, Location
 
 bot = Bot(token=token_for_bot.token_bot)
 dp = Dispatcher(bot)
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
-    await message.reply("Hello! I'm weather telegram bot and I can show actually weather for you. Enter '/spb' for see temperature in St.Petersburg, '/msk' for see temperature in Moscow and ‘/muc’ for see temperature in Munich. If you want to get temperature in your location you should to send your location to weather_bot.")
+    await message.reply("""Hello! I'm weather telegram bot and I can show actually weather for you.
+For read usage information press '/help'""")
+
+@dp.message_handler(commands=['help'])
+async def welcome(message: types.Message):
+    await message.reply("""Press:
+'/spb' for see temperature in St.Petersburg, 
+'/msk' for see temperature in Moscow or 
+'/muc' for see temperature in Munich. 
+If you want to get temperature in your location you should to send your location to weather_bot.
+Let's started!""")
 
 @dp.message_handler(commands=['spb', 'msk', 'muc'])
-async def weather_spb(message: types.Message):
+async def weather(message: types.Message):
     text = get_weather_from_location(message.text[1:])
     await message.reply(text)
 
