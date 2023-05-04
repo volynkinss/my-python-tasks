1f7bbe8e58715c92ece5c0fa43c560aeedfc6ace
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-04 15:47:08 +0300

    added print to terminal and deleted calculations in function

--- weather.py
+++ weather.py
@@ -1,19 +1,23 @@
 import requests
 import json
 
+
 class City:
     def __init__(self, name, latitude, longitude):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
 
+
 list_of_cities = {
-            "spb":City("Saint Petersburg", 59.94, 30.31),
-            "msk":City("Moscow", 55.75, 37.62),
-            "muc":City("Munich", 48.14, 11.58)
-            }
+    "spb": City("Saint Petersburg", 59.94, 30.31),
+    "msk": City("Moscow", 55.75, 37.62),
+    "muc": City("Munich", 48.14, 11.58),
+}
+
 
 def get_location(location):
+    print("start of get_location_function")
     latitude = ""
     longitude = ""
     if type(location) == str:
@@ -21,22 +25,33 @@ def get_location(location):
         request_city = list_of_cities[location]
         latitude = request_city.latitude
         longitude = request_city.longitude
+        geolocation = request_city.name
     elif type(location) == tuple:
         latitude = location[0]
         longitude = location[1]
-    coordinates = {"latitude": latitude, "longitude": longitude, "current_weather":"true"}
-    return coordinates
-        
-    
+        geolocation = "your location"
+    coordinates = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current_weather": "true",
+    }
+    return coordinates, geolocation
+
 
 def get_weather_from_location(location):
+    print("start of get_weather_from_location_function")
     try:
-        reference = requests.get('https://api.open-meteo.com/v1/forecast', params=get_location(location))
-        weather = reference.json()['current_weather']
-        weather_text = f"Temperature in your location now is a {weather['temperature']} degrees Celsius"
+        locate_for_params = get_location(location)[0]
+        reference = requests.get(
+            "https://api.open-meteo.com/v1/forecast", params=locate_for_params
+        )
+        weather = reference.json()["current_weather"]
+        geolocation = get_location(location)[1]
+        weather_text = f"Temperature in {geolocation} now is a {weather['temperature']} degrees Celsius"
         return weather_text
     except Exception as er:
         raise
+
 
 # def get_weather(city):
 #     city = city.lower()
@@ -44,5 +59,3 @@ def get_weather_from_location(location):
 #     latitude = request_city.latitude
 #     longitude = request_city.longitude
 #     get_weather_from_location(latitude , longitude)
-
-
