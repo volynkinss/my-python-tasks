2e478b194cc68c34f8e3b63d2018518af14a2fa2
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-04 14:45:58 +0300

    added function for getting coordinates

--- weather.py
+++ weather.py
@@ -13,25 +13,36 @@ list_of_cities = {
             "muc":City("Munich", 48.14, 11.58)
             }
 
-def get_weather(city):
-    try:
-        city = city.lower()
-        request_city = list_of_cities[city]
-        coordinates = {"latitude": request_city.latitude, "longitude": request_city.longitude, "current_weather":"true"}
-        reference = requests.get('https://api.open-meteo.com/v1/forecast', params=coordinates)
-        weather = reference.json()['current_weather']
-        weather_text = f"Temperature in {request_city.name} now is a {weather['temperature']} degrees Celsius"
-        return weather_text
-    except Exception as er:
-        raise
+def get_location(location):
+    latitude = ""
+    longitude = ""
+    if type(location) == str:
+        location = location.lower()
+        request_city = list_of_cities[location]
+        latitude = request_city.latitude
+        longitude = request_city.longitude
+    elif type(location) == tuple:
+        latitude = location[0]
+        longitude = location[1]
+    coordinates = {"latitude": latitude, "longitude": longitude, "current_weather":"true"}
+    return coordinates
+        
+    
 
-def get_weather_from_location(latitude,longitude):
+def get_weather_from_location(location):
     try:
-        coordinates = {"latitude": latitude, "longitude": longitude, "current_weather":"true"}
-        reference = requests.get('https://api.open-meteo.com/v1/forecast', params=coordinates)
+        reference = requests.get('https://api.open-meteo.com/v1/forecast', params=get_location(location))
         weather = reference.json()['current_weather']
         weather_text = f"Temperature in your location now is a {weather['temperature']} degrees Celsius"
         return weather_text
     except Exception as er:
         raise
+
+# def get_weather(city):
+#     city = city.lower()
+#     request_city = list_of_cities[city]
+#     latitude = request_city.latitude
+#     longitude = request_city.longitude
+#     get_weather_from_location(latitude , longitude)
+
 
