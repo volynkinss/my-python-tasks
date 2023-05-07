ecbbf7f252e4ce6ca87a4e73248148a67e3f755d
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-07 16:38:29 +0300

    edit function of delete msgs

--- telegram_bot_weather.py
+++ telegram_bot_weather.py
@@ -8,35 +8,53 @@ import token_for_bot
 
 bot = Bot(token=token_for_bot.token_bot)
 dp = Dispatcher(bot)
-
-
 print("start of get_weather app")
-
-list_of_messages_id = []
-
-
-async def delete_msgs():  # function that delete unnecessary msgs
-    print("strat of delete_msgs_function")
-    while list_of_messages_id != []:
-        for id in list_of_messages_id:
-            await bot.delete_message(chat_id=chat_id, message_id=id)
-            list_of_messages_id.remove(id)
+base_of_data_chats = {}
 
 
 def add_command_msg_id_to_list(
-    message: types.Message,
+    message: types.Message, chat_id
 ):  # function that add msg id of msg that call command to list_of_messages_id
-    print("strat of add_command_msg_id_to_list_function")
+    print(f"strat of add_command_msg_id_to_list_function for chat:{chat_id}")
     message_id = message.message_id
-    list_of_messages_id.append(message_id)
+    base_of_data_chats[chat_id].append(message_id)
+    print(base_of_data_chats[chat_id])
 
 
 def add_reply_msg_id_to_list(
-    message: types.Message,
+    message: types.Message, chat_id
 ):  # function that add msg id of msg replyed for command to list_of_messages_id
-    print("strat of add_reply_msg_id_to_listfunction")
+    print(f"strat of add_reply_msg_id_to_listfunction for chat:{chat_id}")
     message_id = message.message_id + 1
-    list_of_messages_id.append(message_id)
+    base_of_data_chats[chat_id].append(message_id)
+    print(base_of_data_chats[chat_id])
+
+
+async def delete_msgs(chat_id):  # function that delete unnecessary msgs
+    print(f"strat of delete_msgs_function for chat:{chat_id}")
+    while base_of_data_chats[chat_id] != []:
+        for id in base_of_data_chats[chat_id]:
+            print(f"from {chat_id} deleted {id}")
+            await bot.delete_message(chat_id=chat_id, message_id=id)
+            base_of_data_chats[chat_id].remove(id)
+
+
+@dp.message_handler(commands=["start"])
+async def welcome(message: types.Message):
+    chat_id = message.chat.id
+    print(f"start function of comand 'start'for chat:{chat_id}")
+    if chat_id in base_of_data_chats:
+        pass
+    else:
+        base_of_data_chats[chat_id] = []
+    add_command_msg_id_to_list(message, chat_id)
+    add_reply_msg_id_to_list(message, chat_id)
+    print(base_of_data_chats)
+    await message.reply(
+        f"""Hello! 👋 I'm weather telegram bot and I can get actual weather for you.
+For usage information press '/help'⬅️
+"""
+    )
 
 
 # @dp.message_handler(commands=["end"])
@@ -48,23 +66,12 @@ def add_reply_msg_id_to_list(
 #     await message.reply(messages_id)
 
 
-@dp.message_handler(commands=["start"])
-async def welcome(message: types.Message):
-    global chat_id
-    chat_id = message.chat.id
-    add_command_msg_id_to_list(message)
-    add_reply_msg_id_to_list(message)
-    await message.reply(
-        f"""Hello! 👋 I'm weather telegram bot and I can get actual weather for you.
-For usage information press '/help'⬅️
-"""
-    )
-
-
 @dp.message_handler(commands=["help"])
 async def help(message: types.Message):
-    add_command_msg_id_to_list(message)
-    add_reply_msg_id_to_list(message)
+    chat_id = message.chat.id
+    print(f"start function of comand 'help' for chat:{chat_id}")
+    add_command_msg_id_to_list(message, chat_id)
+    add_reply_msg_id_to_list(message, chat_id)
     await message.reply(
         """Press:
 '/spb' for see t° in St.Petersburg 🏛️, 
@@ -77,24 +84,28 @@ Let's start! 🚀"""
 
 @dp.message_handler(commands=["spb", "msk", "muc"])
 async def handle_city_command(message: types.Message):
-    add_command_msg_id_to_list(message)
+    chat_id = message.chat.id
+    print(f"start function of comand 'spb' or 'msk' or 'muc' for chat:{chat_id}")
+    add_command_msg_id_to_list(message, chat_id)
     city = weather.get_coordinates(message.text)
     temperature = weather.get_weather_from_location(city.latitude, city.longitude)
     text = f"{city.name}'s t° now is a {temperature} °C"
     await message.reply(text)
-    await delete_msgs()
+    await delete_msgs(chat_id)
 
 
 @dp.message_handler(content_types=["location"])
 async def handle_location(message: Message):
-    add_command_msg_id_to_list(message)
+    chat_id = message.chat.id
+    print(f"start function of get user location for chat:{chat_id}")
+    add_command_msg_id_to_list(message, chat_id)
     location = message.location
     temperature = weather.get_weather_from_location(
         location.latitude, location.longitude
     )
     text = f"Temperature in your location now {temperature} °C"
     await message.reply(text)
-    await delete_msgs()
+    await delete_msgs(chat_id)
 
 
 # used functions below to test and learning
@@ -102,21 +113,22 @@ async def handle_location(message: Message):
 
 # @dp.message_handler(commands=["list"])
 # async def list(message: types.Message):
-#     message_id = message.message_id  # id of msg that call  this command
-#     list_of_messages_id.append(message_id)
-#     message_id = message.message_id + 1  # id of msg replyed for this command
-#     list_of_messages_id.append(message_id)
+#     chat_id = message.chat.id
+#     print(f"start function of comand 'list' for chat:{chat_id}")
+#     # message_id = message.message_id  # id of msg that call  this command
+#     # list_of_messages_id.append(message_id)
+#     # message_id = message.message_id + 1  # id of msg replyed for this command
+#     # list_of_messages_id.append(message_id)
 #     await message.reply(
-#         f"""list of messages_id: {list_of_messages_id}
+#         f"""list of messages_id: {base_of_data_chats[chat_id]}
 # """
 #     )
 
 
 # @dp.message_handler(commands=["del"])
 # async def delete(message: types.Message):
-#     message_id = message.message_id  # id of msg that call  this command
-#     list_of_messages_id.append(message_id)
-#     await delete_msgs(list_of_messages_id)
+#     chat_id = message.chat.id
+#     await delete_msgs(chat_id)
 #     await message.reply(
 #         f"""list of messages_id: {list_of_messages_id} deleted
 # """
